e335b1bddd0bcac28b0b250f0aad0ad5bdbb3a24
@Andrew1200 <anragar20@gmail.com>
2024-12-16 14:02:28 +0100

    Colocar imágenes
    Se han colocado imágenes en cada película, comparando el título en el CSV de link_imagenes.csv
    
    Co-Authored-By: Paula Saenz <102244082+paula-saenz@users.noreply.github.com>

--- prueba.py
+++ prueba.py
@@ -6,13 +6,18 @@ def main():
     st.set_page_config(layout="wide")  # Configuración de página
     st.title("Perfil de usuario")  # Título de la página    
 
-    file_path = 'CSV/peliculas_limpio.csv'  # Ruta al archivo CSV
-    data = pd.read_csv(file_path)  # Cargar datos desde el CSV
+    file_path = 'CSV/peliculas_limpio.csv'  # Ruta al archivo CSV de películas
+    data = pd.read_csv(file_path)  # Cargar datos desde el CSV de películas
+
+    # Cargar el CSV con los enlaces de las imágenes
+    image_links_path = 'CSV/link_imagenes.csv'
+    image_links = pd.read_csv(image_links_path)
+
+    # Combinar los datos de películas con los enlaces de imágenes
+    data = pd.merge(data, image_links, on='title', how='left')
 
     # Definir cuántas películas mostrar
-    #que pueda cambiarlo el usuario (hacerlo con un desplegable)
     num_movies = st.sidebar.number_input("Número de películas a mostrar:", min_value=1, max_value=30, value=15)
-    
 
     # Guardar la selección de películas aleatorias en el estado de sesión para evitar que cambien
     if 'random_movies' not in st.session_state:
@@ -39,6 +44,13 @@ def main():
 
             with cols[i]:
                 st.subheader(movie['title'])  # Mostrar título
+                
+                # Mostrar la imagen de la película si está disponible
+                if pd.notna(movie['imagen']):
+                    st.image(movie['image_link'], use_column_width=True)
+                else:
+                    st.write("Imagen no disponible")
+
                 st.write(f"Género: {movie['genre']}")  # Mostrar género
                 st.write(f"Año: {movie['year']}")  # Mostrar año
 
@@ -60,7 +72,6 @@ def main():
         for key, value in st.session_state.items():
             if key.startswith("rating_"):
                 st.write(f"{key}: {value}")
-
 
 if __name__ == "__main__":
     main()
